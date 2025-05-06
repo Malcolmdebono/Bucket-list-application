2731e6358ed5ea0663fa26b69b2202bb17b2c539
pearl733 <pearlbartolo488@gmail.com>
2025-05-06 16:24:00 +0200

    chore: restrict CORS to trusted origins for security

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const cors       = require('cors');
 const jwt        = require('jsonwebtoken');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',  
+    'capacitor://localhost',  
+    'ionic://localhost',    
+    'http://localhost',      
+  ]
+}));
 app.use(express.json());
 
 app.disable('x-powered-by');
